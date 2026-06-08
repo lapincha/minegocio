94e57d87520ccbeb56fc1796ef14031e7fc1e562
minegocio <cuentadesoyecs@gmail.com>
2026-06-08 15:47:10 -0400

    Update carrito.js

--- carrito.js
+++ carrito.js
@@ -1,12 +1,1 @@
-function actualizarContadorCarrito() {
-        const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
-        const totalItems = carrito.reduce((sum, item) => sum + item.cantidad, 0);
-        const contadorSpan = document.getElementById('cart-count');
-        if (contadorSpan) contadorSpan.innerText = totalItems;
-    }
-
-    // Llamar a la función cuando se carga la página
-    document.addEventListener('DOMContentLoaded', actualizarContadorCarrito);
-
-    // (Opcional) si quieres que se actualice cada vez que cambia el localStorage
-    window.addEventListener('storage', actualizarContadorCarrito);
+alert("...");
