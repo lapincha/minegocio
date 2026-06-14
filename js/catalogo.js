d11d5097c360a109f754485a5c0980d68ac8b91f
minegocio <cuentadesoyecs@gmail.com>
2026-06-14 10:12:47 -0400

    Update catalogo.js

--- js/catalogo.js
+++ js/catalogo.js
@@ -5,7 +5,7 @@ const productos = [
   {
     id: 1,
     nombre: "iPhone 16 Pro Max",
-    imagen: "../img/s24.jpg",
+    imagen: "",
     badge: "Tope de gama",
     descripcion: "Pantalla Super Retina XDR de 6.9″, chip A18 Pro, cámara de 48MP con zoom óptico 5x, batería para todo el día y titanio de grado aeroespacial.",
     precio: "$1,199.99",
@@ -14,7 +14,7 @@ const productos = [
   {
     id: 2,
     nombre: "Samsung Galaxy S24 Ultra",
-    imagen: "/img/s24.jpg",
+    imagen: "",
     badge: "Inteligencia IA",
     descripcion: "Pantalla Dynamic AMOLED 2X de 6.8″, Snapdragon 8 Gen 3, S Pen integrado, cámara de 200MP con zoom 100x y Galaxy AI.",
     precio: "$1,299.99",
@@ -23,7 +23,7 @@ const productos = [
   {
     id: 3,
     nombre: "Xiaomi 14 Ultra",
-    imagen: "/s24.jpg",
+    imagen: "",
     badge: "Leica Edition",
     descripcion: "Pantalla AMOLED de 6.73″ 120Hz, Snapdragon 8 Gen 3, sistema de cámara Leica de 4x50MP, batería de 5000mAh y carga rápida de 90W.",
     precio: "$1,099.99",
