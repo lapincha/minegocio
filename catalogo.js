b30df19a0b5a9c1e4124757ec93c9c463ebf821e
minegocio <cuentadesoyecs@gmail.com>
2026-06-15 12:37:08 -0400

    Update catalogo.js

--- catalogo.js
+++ catalogo.js
@@ -1,6 +1,29 @@
 const productos = [
-  // ========== GAMING ==========
+  // ========== DULCES ==========
   { id: 1, categoria: "dulces", nombre: "Dona Glaseada", imagen: "img/dona.jpg", badge: "Esponjosa y dulce", descripcion: "Dona cubierta con glaseado brillante de fresa y chispas de colores, masa suave y aireada.", precio: "$1.99", moneda: "USD" }
+  { 
+  id: 2, 
+  categoria: "dulces", 
+  nombre: "Cupcake de Vainilla y Chispas", 
+  imagen: "img/Cupcake de Vainilla y Chispas.jpeg", 
+  badge: "Más vendido", 
+  descripcion: "Suave bizcocho de vainilla tradicional con un espectacular copete de crema batida y coloridas chispas de dulce.", 
+  precio: "$2.50", 
+  moneda: "USD" 
+  },
+  { 
+  id: 3, 
+  categoria: "reposteria", 
+  nombre: "Galletas con Chispas de Chocolate", 
+  imagen: "img/Galletas con Chispas de Chocolate", 
+  badge: "Artesanales", 
+  descripcion: "Clásicas y deliciosas galletas caseras, horneadas a la perfección con una textura crujiente y cargadas de abundantes chispas de chocolate.", 
+  precio: "$4.50", 
+  moneda: "USD" 
+}
+    
+  
+  
   // ========== SAMSUNG ==========
   
   // ========== IPHONE ==========
